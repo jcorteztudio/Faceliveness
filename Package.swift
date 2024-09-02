01bca27c43740036603db25b62cf5dd0529732c9
jcorteztudio <156157251+jcorteztudio@users.noreply.github.com>
2024-09-02 10:43:12 -0600

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
             targets: ["FaceLivenessFramework"])
     ],
     targets: [
-        .binaryTarget(name: "ISOMDLLib", 
+        .binaryTarget(name: "FaceLivenessFramework", 
                       url: "https://storage.googleapis.com/cosmo-ios-repo/FaceLivenessFramework.xcframework.zip", 
                       checksum: "7d569c6fe5f771eed1ae6a33ff6db6cbc388ab3dde70d6c8679aceb7f8c349e5")
         ],
